e2c02ca93371ac2a4701a0b9edda4e7689e13176
Matthias Langer <mlangc@gmx.at>
2015-04-18 13:45:49 +0200

    Add a self contained example

---
+++ src/main/scala/com/github/mlangc/experiments/SelfContainedExample.scala
@@ -0,0 +1,15 @@
+package com.github.mlangc.experiments
+
+package com.github.mlangc.experiments
+
+package test {
+  class Klass
+}
+
+abstract class AbstractBug {
+  def f: test.Klass
+}
+
+class Bug extends AbstractBug {
+
+}
