60c56583b7eb8799c76dcb3380c06c0baacc095e
Matthias Langer <mlangc@gmx.at>
2015-07-05 16:57:06 +0200

    Update example

--- src/main/scala/com/github/mlangc/experiments/Bug.scala
+++ src/main/scala/com/github/mlangc/experiments/Bug.scala
@@ -1,6 +1,6 @@
 package com.github.mlangc.experiments
 
 class Bug {
-  def f(x: Int) = x
-  def g(a: Int, b: Int) = f(x = a) + b
+  def f(x: Int, y: Int) = x
+  def g(a: Int, b: Int) = f(y = b, x = a)
 }
