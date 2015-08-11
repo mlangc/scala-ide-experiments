fd04c86a7251729d70de058e7c271e76c7920a75
Matthias Langer <matthias.langer@irian.at>
2015-08-11 08:03:00 +0200

    Add illustrating example

--- src/main/scala/com/github/mlangc/experiments/Bug.scala
+++ src/main/scala/com/github/mlangc/experiments/Bug.scala
@@ -1,5 +1,19 @@
 package com.github.mlangc.experiments
 
-class Bug {
+trait Bug {
+  def renameMe: Seq[Kaefer]
+}
+
+class Buggy extends Bug {
+  override def renameMe: Seq[Kaefer] = {
+    ???
+  }
+}
+
+object Kaefer {
+
+}
+
+class Kaefer {
 
 }
