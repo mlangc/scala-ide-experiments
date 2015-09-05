82b72a7bad250bf7990a9b507318ac191630ae2d
Matthias Langer <mlangc@gmx.at>
2015-09-05 20:41:12 +0200

    Add illustrating examples

---
+++ src/main/scala/com/github/mlangc/experiments/Bug1.scala
@@ -0,0 +1,9 @@
+package com.github.mlangc.experiments
+
+import java.lang.String
+
+object O1
+
+trait Bug1 {
+  ")"
+}
---
+++ src/main/scala/com/github/mlangc/experiments/Bug2.scala
@@ -0,0 +1,11 @@
+package com.github.mlangc.experiments
+
+import java.lang.String
+
+object Bug2 {
+  ")"
+}
+
+object O2 {
+
+}
