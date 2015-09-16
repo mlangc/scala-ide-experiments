2ca448cbd74b51d5ea8a3c417f0069dd83591abf
Matthias Langer <matthias.langer@irian.at>
2015-09-16 11:00:27 +0200

    Add illustrating example

--- src/main/scala/com/github/mlangc/experiments/Bug.scala
+++ src/main/scala/com/github/mlangc/experiments/Bug.scala
@@ -1,5 +1,20 @@
 package com.github.mlangc.experiments
 
-class Bug {
+object Bug {
+  object Listings {
+    case class Info(plausiblePrices: Seq[(String, Double)] = Seq(), unplausiblePrices: Seq[(String, Double)] = Seq())
+  }
 
+  case class RegionInfo(regionName: String, listings: Listings.Info)
+
+  case class Listings(
+      forRegion: RegionInfo,
+      forSupRegion: Option[RegionInfo] = None,
+      forNeighbours: Seq[RegionInfo] = Seq())
 }
+
+case class Bug(
+    sqmPrice: Double,
+    regionName: String,
+    listingCategory: String,
+    listings: Bug.Listings)
