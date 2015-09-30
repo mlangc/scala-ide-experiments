41e9d051be0ef8cd5aa64b51b1629ee1f25a22ff
Matthias Langer <mlangc@gmx.at>
2015-09-30 22:19:38 +0200

    Add illustrating example

---
+++ src/main/scala/com/github/mlangc/experiments/TryOrganizeImportsHere.scala
@@ -0,0 +1,8 @@
+package com.github.mlangc.experiments
+
+import scala.collection.immutable.HashMap
+
+object TryOrganizeImportsHere {
+  def values: Seq[HashMap[Int, Int]] = ???
+  List(values: _*)
+}
