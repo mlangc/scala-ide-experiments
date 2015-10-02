e37ed269369eab40c985ed121fde231d35fcc7d2
Matthias Langer <mlangc@gmx.at>
2015-10-02 18:55:29 +0200

    More examples

--- src/main/scala/com/github/mlangc/experiments/Bug.scala
+++ src/main/scala/com/github/mlangc/experiments/Bug.scala
@@ -1,9 +1,26 @@
 package com.github.mlangc.experiments
 
 trait Bug1 {
-  def renameMe: Bug1
+  def renameMe: Seq[Bug1]
 }
 
 trait Bug2 {
   def renameMe: Map[Int, Int]
+}
+
+trait Bug3 {
+  def renameMe: Seq[/**/Int]
+}
+
+trait Bug4 {
+  def renameMe: Seq[Int ]
+}
+
+trait Bug5 {
+  def renameMe: Map[/***/
+    Int, String ]
+}
+
+trait Bug6 {
+  val renameMe: Map[Bug1, Int]
 }
