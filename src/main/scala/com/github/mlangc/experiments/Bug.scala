034b35dae9affc28ca3f786fc4a43f7a1f3735ee
Matthias Langer <mlangc@gmx.at>
2015-10-18 19:33:07 +0200

    Add illustrating example

--- src/main/scala/com/github/mlangc/experiments/Bug.scala
+++ src/main/scala/com/github/mlangc/experiments/Bug.scala
@@ -1,5 +1,33 @@
 package com.github.mlangc.experiments
 
-class Bug {
+object Bug {
+  def main(args: Array[String]): Unit = {
+    sealed trait Step {
+      def back: Step = this match {
+        case StepDown => StepBack(0)
+        case StepBack(level) => StepBack(level + 1)
+      }
 
+      def targetSize(originalSize: Int): Int = this match {
+        case StepDown => originalSize/2
+        case StepBack(level) =>
+          val baseLevel = {
+            if (level == 0) StepDown.targetSize(originalSize)
+            else StepBack(level-1).targetSize(originalSize)
+          }
+
+          val gap = (originalSize - baseLevel)
+          baseLevel + gap/2
+      }
+
+      def isExhausted(originalSize: Int): Boolean = {
+        targetSize(originalSize) == originalSize - 1
+      }
+    }
+
+    case object StepDown extends Step
+    case class StepBack(level: Int) extends Step
+
+    println(StepDown.back)
+  }
 }
