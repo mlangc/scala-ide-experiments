10fcfbb18e6ef53e77f93c7f959547af071d6d49
Matthias Langer <mlangc@gmx.at>
2015-12-08 08:08:06 +0100

    Add illustrating example

--- src/main/scala/com/github/mlangc/experiments/Bug.scala
+++ src/main/scala/com/github/mlangc/experiments/Bug.scala
@@ -1,5 +1,9 @@
 package com.github.mlangc.experiments
 
-class Bug {
+import com.github.mlangc.experiments.bugs.a.b.X
+import com.github.mlangc.experiments.a.c.Y
 
+class Bug {
+  val x: X = ???
+  val y: Y = ???
 }
---
+++ src/main/scala/com/github/mlangc/experiments/bugs/a/b/X.scala
@@ -0,0 +1,5 @@
+package com.github.mlangc.experiments.bugs.a.b
+
+class X {
+  
+}
