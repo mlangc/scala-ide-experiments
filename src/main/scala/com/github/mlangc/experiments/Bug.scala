ea18037fb345cbd593641b60c0f32d3555563daa
Matthias Langer <mlangc@gmx.at>
2015-12-09 20:00:09 +0100

    Add another example

---
+++ src/main/scala/com/github/mlangc/experiments/Bug.scala
@@ -0,0 +1,12 @@
+package com.github.mlangc.experiments
+
+import java.util.Date
+import java.text.SimpleDateFormat
+import java.text.DateFormat
+import java.util.UUID
+import java.io.InputStream
+import java.io.OutputStream
+
+class Bug {
+  def x: (Date, SimpleDateFormat, DateFormat, UUID, InputStream, OutputStream) = ???
+}
