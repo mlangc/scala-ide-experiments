9fbf8845ebbc2db6a07ce3271234e94d18d7579b
Matthias Langer <mlangc@gmx.at>
2015-12-18 10:26:12 +0100

    Add illustrating example

--- src/main/scala/com/github/mlangc/experiments/Bug.scala
+++ src/main/scala/com/github/mlangc/experiments/Bug.scala
@@ -1,5 +1,15 @@
 package com.github.mlangc.experiments
 
-class Bug {
+trait RenameMe {
+  def works1: Map[Int, RenameMe]
+  def works2: Map[Int, /**/RenameMe]
+  def works3: Map[/**/ Int/**/, /**/RenameMe /**/ ]
 
+  def breaksFormat1: Map[Int,RenameMe]
+
+  def breaksCompile1: Map[Int,/**/RenameMe]
+  def breaksCompile2: Map[Int,/**/ RenameMe]
+
+  def breaksCompile3: Map[Int,/**/
+    RenameMe]
 }
