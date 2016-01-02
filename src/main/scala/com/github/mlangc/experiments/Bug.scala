a2b6cb9c6d9fb34f72aafd7c69e3ae923890a591
Matthias Langer <mlangc@gmx.at>
2016-01-02 08:53:03 +0100

    Add illustrating example

--- src/main/scala/com/github/mlangc/experiments/Bug.scala
+++ src/main/scala/com/github/mlangc/experiments/Bug.scala
@@ -1,5 +1,19 @@
 package com.github.mlangc.experiments
 
-class Bug {
+trait ImplicitVals {
+  implicit def x = 42
+}
 
+object Bug {
+  class Ret(x: Int) {
+    def withDefault(a: String = "") = a + x
+  }
+
+  def apply()(implicit x: Int) = {
+    new Ret(x)
+  }
+}
+
+class Bug extends ImplicitVals {
+  val tryRenameMe = Bug().withDefault()
 }
