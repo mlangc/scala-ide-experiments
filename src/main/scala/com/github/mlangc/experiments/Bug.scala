08f9c72002dec5bece8fc5debe3c493064290e10
Matthias Langer <mlangc@gmx.at>
2016-03-24 22:42:58 +0100

    Add example code

--- src/main/scala/com/github/mlangc/experiments/Bug.scala
+++ src/main/scala/com/github/mlangc/experiments/Bug.scala
@@ -1,5 +1,30 @@
 package com.github.mlangc.experiments
 
-class Bug {
+trait SomeTypeClass[T]
 
+object SomeTypeClass {
+  def doSomething[T : SomeTypeClass](t: T) = ???
+}
+
+case class SomeCaseClass(s: String)
+case class SomeGenericCaseClass[T](t: T)
+
+object SomeGenericCaseClass {
+  implicit def someTypeClassForSomeGenericCaseClassImpl[T : SomeTypeClass] = new SomeTypeClass[SomeGenericCaseClass[T]] { }
+}
+
+object SomeCaseClass {
+  implicit val someTypeClassForSomeCaseClassImpl = new SomeTypeClass[SomeCaseClass] { }
+}
+
+object SomeTypeClassImpls {
+  implicit val forInt = new SomeTypeClass[Int] { }
+  implicit val forString = new SomeTypeClass[String] { }
+}
+
+object Bug {
+  import SomeTypeClassImpls._
+
+  val x = SomeTypeClass.doSomething(SomeCaseClass("test"))
+  val y = SomeTypeClass.doSomething(SomeGenericCaseClass(1))
 }
