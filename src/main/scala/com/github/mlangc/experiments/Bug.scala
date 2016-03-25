eb78f278b274f22dfee18fcf158f15a5c57ac806
Matthias Langer <mlangc@gmx.at>
2016-03-25 12:06:16 +0100

    Add illustrating example

--- src/main/scala/com/github/mlangc/experiments/Bug.scala
+++ src/main/scala/com/github/mlangc/experiments/Bug.scala
@@ -1,30 +1,8 @@
 package com.github.mlangc.experiments
 
-trait SomeTypeClass[T]
+import com.github.mlangc.experiments.SomeTypes.InnerType
 
-object SomeTypeClass {
-  def doSomething[T : SomeTypeClass](t: T) = ???
-}
-
-case class SomeCaseClass(s: String)
-case class SomeGenericCaseClass[T](t: T)
-
-object SomeGenericCaseClass {
-  implicit def someTypeClassForSomeGenericCaseClassImpl[T : SomeTypeClass] = new SomeTypeClass[SomeGenericCaseClass[T]] { }
-}
-
-object SomeCaseClass {
-  implicit val someTypeClassForSomeCaseClassImpl = new SomeTypeClass[SomeCaseClass] { }
-}
-
-object SomeTypeClassImpls {
-  implicit val forInt = new SomeTypeClass[Int] { }
-  implicit val forString = new SomeTypeClass[String] { }
-}
-
-object Bug {
-  import SomeTypeClassImpls._
-
-  val x = SomeTypeClass.doSomething(SomeCaseClass("test"))
-  val y = SomeTypeClass.doSomething(SomeGenericCaseClass(1))
+class Bug {
+  val ic: InnerClass = ???
+  val it: InnerType = ???
 }
---
+++ src/main/scala/com/github/mlangc/experiments/SomeTypes.scala
@@ -0,0 +1,6 @@
+package com.github.mlangc.experiments
+
+object SomeTypes {
+  class InnerClass
+  type InnerType = String
+}
