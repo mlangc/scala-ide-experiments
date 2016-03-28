cce45bca5bc2e46ad497aecbecb686f696a65e74
Matthias Langer <mlangc@gmx.at>
2016-03-28 13:28:43 +0200

    Enhance example

--- src/main/java/com/github/mlangc/experiments/AnAnnotation.java
+++ src/main/java/com/github/mlangc/experiments/AnAnnotation.java
@@ -3,4 +3,5 @@ package com.github.mlangc.experiments;
 public @interface AnAnnotation {
 	Class<?> value1();
 	Class<?> value2();
+	int value3();
 }
--- src/main/scala/com/github/mlangc/experiments/Bug.scala
+++ src/main/scala/com/github/mlangc/experiments/Bug.scala
@@ -5,11 +5,18 @@ import org.junit.runner.RunWith
 import org.scalatest.junit.JUnitRunner
 import com.github.mlangc.experiments.test._
 
+/*
+ * Try renaming or organizing imports here
+ */
 package test {
-  class Foo
-  class Bar
+  class TryRenameMe
+  class OrTryRenameMe
+
+  object Constants {
+    final val OrTryRenameMe = 42
+  }
 }
 
 @RunWith(classOf[JUnitRunner])
-@AnAnnotation(value1 = classOf[Foo], value2 = classOf[Bar])
+@AnAnnotation(value1 = classOf[TryRenameMe], value2 = classOf[OrTryRenameMe], value3 = Constants.OrTryRenameMe)
 class Bug
