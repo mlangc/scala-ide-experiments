c408b2d3c0d393163c1bbc41a54e16c0ceb8a009
Matthias Langer <mlangc@gmx.at>
2016-03-28 13:34:41 +0200

    Get rid of external dependencies

--- build.sbt
+++ build.sbt
@@ -8,10 +8,6 @@ scalaVersion := "2.11.8"
 
 scalacOptions := Seq("-encoding", "utf8", "-target:jvm-1.8")
 
-libraryDependencies += "junit" % "junit" % "4.12"
-
-libraryDependencies += "org.scalatest" %% "scalatest" % "2.2.4"
-
 EclipseKeys.withSource := true
 
 EclipseKeys.createSrc := EclipseCreateSrc.Default + EclipseCreateSrc.Resource
--- src/main/scala/com/github/mlangc/experiments/Bug.scala
+++ src/main/scala/com/github/mlangc/experiments/Bug.scala
@@ -1,8 +1,5 @@
 package com.github.mlangc.experiments
 
-import org.junit.runner.RunWith
-
-import org.scalatest.junit.JUnitRunner
 import com.github.mlangc.experiments.test._
 
 /*
@@ -17,6 +14,5 @@ package test {
   }
 }
 
-@RunWith(classOf[JUnitRunner])
 @AnAnnotation(value1 = classOf[TryRenameMe], value2 = classOf[OrTryRenameMe], value3 = Constants.OrTryRenameMe)
 class Bug
