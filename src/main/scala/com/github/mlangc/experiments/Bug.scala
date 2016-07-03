96b7c37a03ea2d77c4ec77f46f2080b6f5826263
Matthias Langer <mlangc@gmx.at>
2016-07-03 20:05:44 +0200

    Add illustrating example

--- src/main/scala/com/github/mlangc/experiments/Bug.scala
+++ src/main/scala/com/github/mlangc/experiments/Bug.scala
@@ -1,5 +1,8 @@
 package com.github.mlangc.experiments
 
-class Bug {
-
+object Bug {
+  sealed trait Expr 
+  case class Var(name: String) extends Expr
+  case class Lambda(arg: Var, body: Expr) extends Expr
+  case class Apply(fun: Expr, arg: Expr) extends Expr 
 }
