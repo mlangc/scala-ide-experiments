85021761763ecf075f8d51e4f5d7fb1ce13ec347
Matthias Langer <mlangc@gmx.at>
2016-07-08 16:46:15 +0200

    Refine example for #1002705

--- src/main/scala/com/github/mlangc/experiments/Bug.scala
+++ src/main/scala/com/github/mlangc/experiments/Bug.scala
@@ -7,11 +7,13 @@ class Bug {
   val d = 1 +: 2 +: 3 +: Seq()
   val e = 1.0 + 2.0 + 3.0
   val f = "1" + "2" + "3"
-  
+
   val g = someList match {
+    case _ :: _ :: x3 :: _ :: Nil => x3
+    case _ :: _ :: x3 :: _ => x3 * 2
     case x :: xs => x
     case _ => Nil
   }
-  
+
   def someList: List[Int] = 1 :: 2 :: 3 :: Nil
 }
