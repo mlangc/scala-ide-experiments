1529b4dc7723ca2e7a26ab6546d34b4770da3540
Matthias Langer <mlangc@gmx.at>
2016-09-03 07:38:14 +0200

    Extend example

---
+++ src/main/scala/com/github/mlangc/experiments/move/me/here/UseMoveClass.scala
@@ -0,0 +1,8 @@
+package com.github.mlangc.experiments.move.me.here
+
+import com.github.mlangc.experiments.MoveMe
+
+class UseMoveClass {
+  val use = new MoveMe
+
+}
---
+++ src/main/scala/com/github/mlangc/experiments/used/from/here/UseMovedClass.scala
@@ -0,0 +1,7 @@
+package com.github.mlangc.experiments.used.from.here
+
+import com.github.mlangc.experiments.MoveMe
+
+class UseMovedClass {
+  val use = new MoveMe
+}
